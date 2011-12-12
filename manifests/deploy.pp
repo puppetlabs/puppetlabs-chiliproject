80addbbe28f4279af3be3f41d77b88e0d33a27fb
Daniel Sauble <djsauble@puppetlabs.com>
2011-12-12 14:35:44 -0800

    Rough implementation for project empathy
    - manifests/configuration.pp amended to include email delivery options
    - configuration.yml moved to a template (configuration.yml.erb)
    - manifests/data.pp amended to include email delivery defaults
    - manifests/deploy.pp amended to include passenger and plugin support
    - postgresql server/client packages added to manifests/init.pp
    - database setup added to manifests/init.pp
    - manifests/repo.pp changed so user is not hardcoded and production log
      gets correct permissions

--- manifests/deploy.pp
+++ manifests/deploy.pp
@@ -11,12 +11,15 @@
 # Sample Usage:
 #
 class chiliproject::deploy(
-  $path = $chiliproject::data::path
+  $path     = $chiliproject::data::path,
+  $port     = 3000,
+  $pid_file = "$path/tmp/pids/passenger.$port.pid",
+  $user     = "puppet"
 ) {
 
   exec { 'chiliproject_bundle_install':
     command     => 'bundle install --without=test mysql mysql2 sqlite openid',
-    path        => '/usr/bin:/bin:/opt/puppet/bin:/var/lib/gems/1.8/bin',
+    path        => '/usr/local/bin:/usr/bin:/bin:/opt/puppet/bin:/var/lib/gems/1.8/bin',
     cwd         => $path,
     refreshonly => true,
     subscribe   => Class['chiliproject::repo'],
@@ -24,29 +27,57 @@ class chiliproject::deploy(
 
   exec { 'chiliproject_migrate':
     command     => 'bundle exec rake db:migrate',
-    path        => '/usr/bin:/bin:/opt/puppet/bin:/var/lib/gems/1.8/bin',
+    path        => '/usr/local/bin:/usr/bin:/bin:/opt/puppet/bin:/var/lib/gems/1.8/bin',
     environment => 'RAILS_ENV=production',
     cwd         => $path,
     refreshonly => true,
     subscribe   => Class['chiliproject::repo'],
-    require     => Exec['chiliproject_bundle_install'],
+    require     => Exec['gen_session_store'],
   }
 
   exec { 'gen_session_store':
     command     => 'bundle exec rake generate_session_store',
-    path        => '/usr/bin:/bin:/opt/puppet/bin:/var/lib/gems/1.8/bin',
+    path        => '/usr/local/bin:/usr/bin:/bin:/opt/puppet/bin:/var/lib/gems/1.8/bin',
     environment => 'RAILS_ENV=production',
     cwd         => $path,
     creates     => "${path}/config/initializers/session_store.rb",
-    require     => [ Class['chiliproject::repo'], Exec['chiliproject_bundle_install'] ],
+    require     => Exec['chiliproject_bundle_install']
   }
 
   exec { 'default_data':
     command     => 'bundle exec rake redmine:load_default_data',
-    path        => '/usr/bin:/bin:/opt/puppet/bin:/var/lib/gems/1.8/bin',
-    environment => [ 'REDMINE_LANG=en', 'RAILS_ENV=production' ],
+    path        => '/usr/local/bin:/usr/bin:/bin:/opt/puppet/bin:/var/lib/gems/1.8/bin',
+    environment => ['REDMINE_LANG=en', 'RAILS_ENV=production'],
     cwd         => $path,
-    require     => [ Class['chiliproject::repo'], Exec['chiliproject_migrate'] ],
+    require     => Exec['chiliproject_migrate']
+  }
+  
+  file { "$path/vendor":
+    ensure  => directory,
+    recurse => true,
+    source  => 'puppet:///modules/chiliproject/vendor' 
   }
 
+  exec { 'plugins':
+    command     => 'bundle exec rake db:migrate:plugins',
+    path        => '/usr/local/bin:/usr/bin:/bin:/opt/puppet/bin:/var/lib/gems/1.8/bin',
+    environment => 'RAILS_ENV=production',
+    cwd         => $path,
+    require     => File["$path/vendor"]
+  }
+
+  package { 'passenger':
+    provider    => 'gem',
+    name        => 'passenger',
+    ensure      => 'latest'
+  }
+
+  exec { 'passenger':
+    command     => "passenger start --port $port --pid-file $pid_file --user $user --daemonize",
+    creates     => "$pid_file",
+    path        => '/usr/local/bin:/usr/bin:/bin:/opt/puppet/bin:/var/lib/gems/1.8/bin',
+    environment => 'RAILS_ENV=production',
+    cwd         => $path,
+    require     => [File["$path/vendor"], Package['passenger']]
+  }
 }
--- manifests/repo.pp
+++ manifests/repo.pp
@@ -11,24 +11,34 @@
 # Sample Usage:
 #
 class chiliproject::repo(
+  $user = $chiliproject::data::user,
   $staging_dir = $chiliproject::data::staging_dir,
   $repo_source = $chiliproject::data::repo_source,
   $git_revision = $chiliproject::data::git_revision
 ) {
-
   file { $staging_dir:
     ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0600',
+    owner  => $user,
+    group  => $user,
+    mode   => "0600",
   }
 
-  vcsrepo { 'chiliproject_repo':
+  vcsrepo { "chiliproject_repo":
     path     => $staging_dir,
-    ensure   => $git_revision,
+    owner    => $user,
+    group    => $user,
+    ensure   => present,
+    force    => true,
     source   => $repo_source,
-    provider => 'git',
-    require  => Class['chiliproject'],
+    revision => $git_revision,
+    provider => "git",
+    require  => Class["chiliproject"],
   }
 
+  # Needed because the rake db:migrate command is returning 1 instead of 0
+  file { "$staging_dir/log/production.log":
+    ensure  => present,
+    mode    => "0666",
+    require => Vcsrepo["chiliproject_repo"],
+  }
 }
